24e063d0a3e9c374362808dfaae291a9c67245d8
palmmicro <woody@palmmicro.com>
2018-05-02 00:34:11 +0800

    20180426cn.php

--- chishin/php/_navchishin.php
+++ chishin/php/_navchishin.php
@@ -2,10 +2,11 @@
 require_once('/php/blogcomments.php');
 require_once('/php/layout.php');
 require_once('/php/nav.php');
+require_once('/php/ui/link.php');
 
 function ChishinNavigateBlogGroup($bChinese)
 {
-    $arBlog = array('20170512', '20170517', '20170523', '20170524', '20180429');
+    $arBlog = array('20170512', '20170517', '20170523', '20170524', '20180426', '20180429');
     
 	NavBegin();
     NavDirFirstLast($arBlog);
