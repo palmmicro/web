8bd308399f008a590dbbe96e85ec62a35aa3b859
woody <woody@palmmicro.com>
2021-02-28 15:47:52 +0800

    DirectReply

--- php/telegram.php
+++ php/telegram.php
@@ -4,13 +4,14 @@ require_once('debug.php');
 require_once('stockbot.php');
 
 // 电报公共模板, 返回输入信息
-define('TG_DEBUG_VER', '版本001');		
+define('TG_DEBUG_VER', '版本007');		
 
 define('BOT_EOL', "\r\n");
 define('MAX_BOT_MSG_LEN', 2048);
 
 define('TG_API_URL', 'https://api.telegram.org/bot'.TG_TOKEN.'/');
-define('TG_ADMIN_CHAT_ID', '992671436');
+define('TG_ADMIN_CHAT_ID', '992671436');		// @sz152
+define('TG_CAST_CHAT_ID', '-1001346320717');	// @palmmicrocast
 
 class TelegramCallback
 {
@@ -28,6 +29,25 @@ class TelegramCallback
 		}
 	}
 
+	function DirectReply($method, $parameters) 
+	{
+		if (!is_string($method))			return false; 
+		if (!$parameters) 		    		$parameters = array();
+		else if (!is_array($parameters))	return false;
+
+		$parameters['method'] = $method;
+		$payload = json_encode($parameters);
+		header('Content-Type: application/json');
+		header('Content-Length: '.strlen($payload));
+		echo $payload;
+		return true;
+	}
+
+	function ReplyText($strText, $strMessageId, $strChatId) 
+	{
+		$this->DirectReply('sendMessage', array('chat_id' => $strChatId, 'reply_to_message_id' => $strMessageId, 'text' => $strText));
+	}
+	
 	function _sendText($strText, $chat_id) 
 	{
         url_get_contents(TG_API_URL.'sendmessage?text='.urlencode($strText).'&chat_id='.$chat_id);        //valid signature , option
@@ -36,9 +56,10 @@ class TelegramCallback
 	function Debug($strDebug)
 	{
 		$this->_sendText($strDebug, TG_ADMIN_CHAT_ID);
+//		$this->_sendText($strDebug, TG_CAST_CHAT_ID);
 	}
 	
-    function OnText($strText, $strChatId)
+    function OnText($strText, $strMessageId, $strChatId)
     {
 		$this->_sendText($strText, $strChatId);
     }
@@ -64,7 +85,7 @@ class TelegramCallback
 					break;
 					
 				default:
-					$this->OnText($strCmd, $chat_id);
+					$this->OnText($strCmd, $message_id, $chat_id);
 					break;
 				}
 			} 
@@ -72,14 +93,13 @@ class TelegramCallback
 			{
 //				$name = $message['from']['first_name'];
 //				$strText = $text.' '.$name;
-				$this->OnText($text, $chat_id);
-//			      apiRequestWebhook("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => 'Cool'));
+				$this->OnText($text, $message_id, $chat_id);
 			}
 		}
 		else 
 		{
 //			apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
-//			$this->_sendText('只能回复文本消息', $chat_id);
+			$this->_sendText('只能回复文本消息', $chat_id);
 		}
 	}		
 
@@ -104,11 +124,13 @@ class TelegramStock extends TelegramCallback
     	SqlConnectDatabase();
     }
 
-    function OnText($strText, $strChatId)
+    function OnText($strText, $strMessageId, $strChatId)
     {
-        if ($str = StockBotGetStr($strText, $this->GetVersion()))
+    	$strVersion = $this->GetVersion();
+        if ($str = StockBotGetStr($strText, $strVersion))
         {
-        	$this->_sendText($str, $strChatId);
+			$str .= $strVersion; 
+        	$this->ReplyText($str, $strMessageId, $strChatId);
         }
         else
         {
